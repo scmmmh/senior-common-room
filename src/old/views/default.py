081cba649518960f768c507125b9b4aeb9a52a54
Mark Hall <mark.hall@work.room3b.eu>
2020-10-15 14:52:13 +0100

    Remove old code

--- src/old/views/default.py
+++
@@ -1,9 +0,0 @@
-from pyramid.view import view_config
-
-from ..session import require_logged_in
-
-
-@view_config(route_name='root', renderer='scr:templates/root.jinja2')
-@require_logged_in()
-def root(request):
-    return {}
