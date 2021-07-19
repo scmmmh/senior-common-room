ec25268293de1d26eaaa1e820529bdfd0c0457d4
Mark <mark.hall@work.room3b.eu>
2021-07-19 21:33:11 +0100

    Support arbitrary first room slugs

--- src/frontend/src/store/config.ts
+++ src/frontend/src/store/config.ts
@@ -1,4 +1,5 @@
-import { writable } from "svelte/store";
+import { writable, get } from "svelte/store";
+import { navigate } from 'svelte-navigator';
 
 import { messages, sendMessage } from './connection';
 
@@ -10,6 +11,10 @@ messages.subscribe((message) => {
             type: 'get-rooms-config'
         });
     } else if (message.type === 'rooms-config') {
+        const firstLoad = (get(rooms).length === 0);
         rooms.set(message.payload as RoomConfigPayload[]);
+        if (firstLoad && (message.payload as RoomConfigPayload[]).length > 0) {
+            navigate('/frontend/room/' + (message.payload as RoomConfigPayload[])[0].slug);
+        }
     }
 });
