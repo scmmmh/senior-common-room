1c2bebe8d070055479f8da78aad5cd97209e649e
Mark Hall <mark.hall@work.room3b.eu>
2021-08-13 15:26:04 +0100

    v0.1.7

---
+++ prepare_release.py
@@ -0,0 +1,29 @@
+import re
+
+
+VERSION = '0.1.7'
+
+
+def readlines(filename: str) -> list[str]:
+    with open(filename) as in_f:
+        return in_f.readlines()
+
+
+def writelines(filename: str, lines: list[str]):
+    with open(filename, 'w') as out_f:
+        out_f.write(''.join(lines))
+
+
+def update_version(filename: str, pattern: str, version: str):
+    def replace_version(line: str) -> str:
+        if re.match(pattern, line):
+            return re.sub('[0-9]+\.[0-9]+\.[0-9]+', version, line)
+        else:
+            return line
+
+    writelines(filename, map(replace_version, readlines(filename)))
+
+
+update_version('src/frontend/package.json', '^  "version": "[0-9]+\.[0-9]+\.[0-9]",$', VERSION)
+update_version('pyproject.toml', '^version = "[0-9]+\.[0-9]+\.[0-9]+"$',  VERSION)
+update_version('docker/production/Dockerfile', '^.*senior_common_room-[0-9]+\.[0-9]+\.[0-9]+-py3.*$', VERSION)
