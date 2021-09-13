d5456e0aed8537b98ba43f30337b56e33806ded9
Mark Hall <mark.hall@work.room3b.eu>
2021-09-13 08:51:15 +0100

    Use padding instead of margin

--- src/frontend/src/components/Admin.svelte
+++ src/frontend/src/components/Admin.svelte
@@ -30,7 +30,7 @@
     }
 </script>
 
-<div class="fixed right-0 top-0 h-screen mt-20 pb-4 z-40">
+<div class="fixed right-0 top-0 h-screen pt-20 pb-4 z-40">
     <div class="bg-gray-700 text-white rounded-l-lg py-4 pl-4 pr-2">
         <h2 class="sr-only">Administration</h2>
         <div class="{visible ? 'mb-4' : ''}">
